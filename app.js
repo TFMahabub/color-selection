7efc50c007a95a6b9d2056c0755f85285bdffcc6
TFMahabub <rjmahabub543@gmail.com>
2022-08-17 09:56:28 +0600

    first commit

---
+++ app.js
@@ -0,0 +1,19 @@
+const colors = ['red', 'green', 'purple', 'pink', 'black'];
+
+const container = document.getElementById('container')
+const title = document.querySelector('h2');
+const selectedColor = document.getElementById('selectedColor')
+
+for(let color of colors){
+  const newDivs = document.createElement('div');
+  newDivs.classList.add('box');
+  container.appendChild(newDivs)
+
+  newDivs.style.backgroundColor = color;
+
+  newDivs.addEventListener('click', function(){
+    title.innerText = 'You have selected: ';
+    selectedColor.innerText = newDivs.style.backgroundColor;
+    selectedColor.style.backgroundColor = newDivs.style.backgroundColor;
+  })
+}
